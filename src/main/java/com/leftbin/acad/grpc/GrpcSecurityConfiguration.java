ca79f083f7c00808b2fe8f496d821e49bac43c44
swarupdonepudi <swarup.donepudi@gmail.com>
2020-10-05 19:10:48 -0700

    added not working version

---
+++ src/main/java/com/leftbin/acad/grpc/GrpcSecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.leftbin.acad.grpc;
+
+import org.lognet.springboot.grpc.security.EnableGrpcSecurity;
+import org.lognet.springboot.grpc.security.GrpcSecurityConfigurerAdapter;
+
+@EnableGrpcSecurity
+public class GrpcSecurityConfiguration extends GrpcSecurityConfigurerAdapter {
+
+}
