cfa63a65dc7767de80810998ef3c119a5fbeca08
swarupdonepudi <swarup.donepudi@gmail.com>
2020-10-06 13:24:45 -0700

    updated the nimbus version to 9.0.1 as per github issue https://github.com/spring-projects/spring-security/issues/9040

--- src/main/java/com/leftbin/acad/grpc/GrpcSecurityConfiguration.java
+++ src/main/java/com/leftbin/acad/grpc/GrpcSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.lognet.springboot.grpc.security.EnableGrpcSecurity;
 import org.lognet.springboot.grpc.security.GrpcSecurity;
 import org.lognet.springboot.grpc.security.GrpcSecurityConfigurerAdapter;
 import org.lognet.springboot.grpc.security.jwt.JwtAuthProviderFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,7 @@ import org.springframework.security.oauth2.jwt.*;
 @Configuration
 @EnableGrpcSecurity
 public class GrpcSecurityConfiguration extends GrpcSecurityConfigurerAdapter {
+    Logger l = LoggerFactory.getLogger(GrpcSecurityConfiguration.class);
     @Value("${auth0.audience}")
     private String audience;
 
@@ -23,9 +26,9 @@ public class GrpcSecurityConfiguration extends GrpcSecurityConfigurerAdapter {
 
     @Bean
     JwtDecoder jwtDecoder() {
+        l.info("swarup: invoked jwtDecoder method to instantiate jwtDecoder");
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
                 JwtDecoders.fromOidcIssuerLocation(issuer);
-
         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
         OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
@@ -38,8 +41,7 @@ public class GrpcSecurityConfiguration extends GrpcSecurityConfigurerAdapter {
     @Override
     public void configure(GrpcSecurity builder) throws Exception {
         builder.authorizeRequests()
-                .methods(GreeterGrpc.getSayHelloMethod()).hasAnyAuthority("SCOPE_profile")
-                .and()
+                .methods(GreeterGrpc.getSayHelloMethod()).authenticated().withSecuredAnnotation()
                 .authenticationProvider(JwtAuthProviderFactory.forAuthorities(jwtDecoder()));
     }
 
